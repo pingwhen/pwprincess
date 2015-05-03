dd15904314f6bcdb41f1538abf8eb639d0d47b84
Sam Heather <sam@heather.sh>
2015-05-03 18:29:17 +0100

    Removed duplicated js imports

--- footer.php
+++ footer.php
@@ -88,3 +88,29 @@ ABC
 						</div>
 					</div>
 				</footer>
+				
+<!-- Vendor -->
+<script src="vendor/jquery/jquery.js"></script>
+<script src="vendor/jquery.appear/jquery.appear.js"></script>
+<script src="vendor/jquery.easing/jquery.easing.js"></script>
+<script src="vendor/jquery-cookie/jquery-cookie.js"></script>
+<script src="vendor/bootstrap/bootstrap.js"></script>
+<script src="vendor/common/common.js"></script>
+<script src="vendor/jquery.validation/jquery.validation.js"></script>
+<script src="vendor/jquery.stellar/jquery.stellar.js"></script>
+<script src="vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.js"></script>
+<script src="vendor/jquery.gmap/jquery.gmap.js"></script>
+<script src="vendor/isotope/jquery.isotope.js"></script>
+<script src="vendor/owlcarousel/owl.carousel.js"></script>
+<script src="vendor/jflickrfeed/jflickrfeed.js"></script>
+<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+<script src="vendor/vide/vide.js"></script>
+
+<!-- Theme Base, Components and Settings -->
+<script src="js/theme.js"></script>
+
+<!-- Theme Custom -->
+<script src="js/custom.js"></script>
+
+<!-- Theme Initialization Files -->
+<script src="js/theme.init.js"></script>
--- values.php
+++ values.php
@@ -164,47 +164,5 @@
         <!-- Footer -->
 		<?php include("footer.php"); ?>
 
-		<!-- Vendor -->
-		<script src="vendor/jquery/jquery.js"></script>
-		<script src="vendor/jquery.appear/jquery.appear.js"></script>
-		<script src="vendor/jquery.easing/jquery.easing.js"></script>
-		<script src="vendor/jquery-cookie/jquery-cookie.js"></script>
-		<script src="vendor/bootstrap/bootstrap.js"></script>
-		<script src="vendor/common/common.js"></script>
-		<script src="vendor/jquery.validation/jquery.validation.js"></script>
-		<script src="vendor/jquery.stellar/jquery.stellar.js"></script>
-		<script src="vendor/jquery.easy-pie-chart/jquery.easy-pie-chart.js"></script>
-		<script src="vendor/jquery.gmap/jquery.gmap.js"></script>
-		<script src="vendor/isotope/jquery.isotope.js"></script>
-		<script src="vendor/owlcarousel/owl.carousel.js"></script>
-		<script src="vendor/jflickrfeed/jflickrfeed.js"></script>
-		<script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
-		<script src="vendor/vide/vide.js"></script>
-		
-		<!-- Theme Base, Components and Settings -->
-		<script src="js/theme.js"></script>
-		
-		<!-- Theme Custom -->
-		<script src="js/custom.js"></script>
-		
-		<!-- Theme Initialization Files -->
-		<script src="js/theme.init.js"></script>
-
-		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
-		<script type="text/javascript">
-		
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-12345678-1']);
-			_gaq.push(['_trackPageview']);
-		
-			(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
-		
-		</script>
-		 -->
-
 	</body>
 </html>

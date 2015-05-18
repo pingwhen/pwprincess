9d277ed1bedb9c4d435af2f1ade0be265d038766
Sam Heather <sam@heather.sh>
2015-05-19 00:48:09 +0100

    Factored out common header, added LinkedIn link

--- header.php
+++ header.php
@@ -1,55 +1,107 @@
-<header id="header">
-    <div class="container">
-        <div class="logo">
-            <a href="/">
-                <img alt="Porto" width="140" height="70" src="img/logo.png">
-            </a>
-        </div>
-        <button class="btn btn-responsive-nav btn-inverse" data-toggle="collapse" data-target=".nav-main-collapse">
-            <i class="fa fa-bars"></i>
-        </button>
-    </div>
-    <div class="navbar-collapse nav-main-collapse collapse">
-        <div class="container">
-            <ul class="social-icons">
-                <li class="facebook"><a href="https://www.facebook.com/pages/Pingwhen/292317570939511#" target="_blank" title="Facebook">Facebook</a></li>
-                <li class="twitter"><a href="http://www.twitter.com/pingwhenapp" target="_blank" title="Twitter">Twitter</a></li>
-                <li class="linkedin"><a href="http://www.linkedin.com/" target="_blank" title="Linkedin">Linkedin</a></li>
-            </ul>
-            <nav class="nav-main mega-menu">
-                <ul class="nav nav-pills nav-main" id="mainMenu">
-                    <li class="dropdown active">
-                        <a class="dropdown-toggle" href="/">
-                            The App
-                            <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="/">Features</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" href="about">
-                            About Us
-                            <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="about">About Us</a></li>
-                            <li><a href="team">Our Team</a></li>
-                            <li><a href="values">Our Values</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" href="#">
-                            Join Us
-                            <i class="fa fa-angle-down"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a href="/">Beta Tester<span class="tip">Exclusive</span></a></li>
-                            <li><a href="contact">Contact Us</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Basic -->
+		<meta charset="utf-8">
+		<title>pingWHEN | About Us</title>		
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Porto - Responsive HTML5 Template">
+		<meta name="author" content="okler.net">
+
+		<!-- Mobile Metas -->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<!-- Web Fonts  -->
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light" rel="stylesheet" type="text/css">
+
+		<!-- Vendor CSS -->
+		<link rel="stylesheet" href="vendor/bootstrap/bootstrap.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.css">
+		<link rel="stylesheet" href="vendor/owlcarousel/owl.carousel.min.css" media="screen">
+		<link rel="stylesheet" href="vendor/owlcarousel/owl.theme.default.min.css" media="screen">
+		<link rel="stylesheet" href="vendor/magnific-popup/magnific-popup.css" media="screen">
+
+		<!-- Theme CSS -->
+		<link rel="stylesheet" href="css/theme.css">
+		<link rel="stylesheet" href="css/theme-elements.css">
+		<link rel="stylesheet" href="css/theme-blog.css">
+		<link rel="stylesheet" href="css/theme-shop.css">
+		<link rel="stylesheet" href="css/theme-animate.css">
+
+		<!-- Skin CSS -->
+		<link rel="stylesheet" href="css/skins/default.css">
+
+		<!-- Theme Custom CSS -->
+		<link rel="stylesheet" href="css/custom.css">
+
+		<!-- Head Libs -->
+		<script src="vendor/modernizr/modernizr.js"></script>
+
+		<!--[if IE]>
+			<link rel="stylesheet" href="css/ie.css">
+		<![endif]-->
+
+		<!--[if lte IE 8]>
+			<script src="vendor/respond/respond.js"></script>
+			<script src="vendor/excanvas/excanvas.js"></script>
+		<![endif]-->
+
+	</head>
+	<body>
+
+	<header id="header">
+		<div class="container">
+			<div class="logo">
+				<a href="/">
+					<img alt="Porto" width="140" height="70" src="img/logo.png">
+				</a>
+			</div>
+			<button class="btn btn-responsive-nav btn-inverse" data-toggle="collapse" data-target=".nav-main-collapse">
+				<i class="fa fa-bars"></i>
+			</button>
+		</div>
+		<div class="navbar-collapse nav-main-collapse collapse">
+			<div class="container">
+				<ul class="social-icons">
+					<li class="facebook"><a href="https://www.facebook.com/pages/Pingwhen/292317570939511#" target="_blank" title="Facebook">Facebook</a></li>
+					<li class="twitter"><a href="http://www.twitter.com/pingwhenapp" target="_blank" title="Twitter">Twitter</a></li>
+					<li class="linkedin"><a href="https://www.linkedin.com/company/9442822" target="_blank" title="Linkedin">Linkedin</a></li>
+				</ul>
+				<nav class="nav-main mega-menu">
+					<ul class="nav nav-pills nav-main" id="mainMenu">
+						<li class="dropdown active">
+							<a class="dropdown-toggle" href="/">
+								The App
+								<i class="fa fa-angle-down"></i>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="/">Features</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a class="dropdown-toggle" href="about">
+								About Us
+								<i class="fa fa-angle-down"></i>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="about">About Us</a></li>
+								<li><a href="team">Our Team</a></li>
+								<li><a href="values">Our Values</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a class="dropdown-toggle" href="#">
+								Join Us
+								<i class="fa fa-angle-down"></i>
+							</a>
+							<ul class="dropdown-menu">
+								<li><a href="/">Beta Tester<span class="tip">Exclusive</span></a></li>
+								<li><a href="contact">Contact Us</a></li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>

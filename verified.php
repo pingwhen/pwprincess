71184ea2bdffdbbcecf34f0950ac648305e2084a
Sam Heather <sam@heather.sh>
2015-06-25 14:02:01 -0600

    Added email verified confirmation

---
+++ verified.php
@@ -0,0 +1,43 @@
+            <!-- Header -->
+	        <?php include("header.php"); ?>
+
+			<div role="main" class="main">
+
+				<section class="page-top">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<ul class="breadcrumb">
+									<li><a href="/">Home</a></li>
+									<li class="active">Email Verification</li>
+								</ul>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h1>Email Verification</h1>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+					<h2>Email Verified</strong></h2>
+
+					<div class="row">
+						<div class="col-md-12">
+							<p class="lead">
+								Thanks for verifying your email address. Travel safe!
+							</p>
+						</div>
+					<hr>
+				</div>
+			</div>
+		</div>
+
+        <!-- Footer -->
+		<?php include("footer.php"); ?>
+
+	</body>
+</html>
